d280218979c2992fd05bdba76f04d06bbe1c0000
jeongmin <yesuel1111@naver.com>
2023-03-08 13:28:55 +0900

    fix: s is NULL in ft_error_exit

--- ft_func/error_exit.c
+++ ft_func/error_exit.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   error_exit.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jeongmin <jeongmin@student.42.fr>          +#+  +:+       +#+        */
+/*   By: jeongmin <jeongmin@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/26 20:32:48 by jeongmin          #+#    #+#             */
-/*   Updated: 2023/03/05 19:00:10 by jeongmin         ###   ########.fr       */
+/*   Updated: 2023/03/08 13:27:15 by jeongmin         ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,10 +26,13 @@ void	ft_error_exit(const char *s, t_param *param)
 {
 	if (write(2, "Error\n", 6) == -1)
 		ft_perror_exit("write error", param);
-	if (write(2, s, ft_strlen(s)) == -1)
-		ft_perror_exit("write error", param);
-	if (write(2, "\n", 1) == -1)
-		ft_perror_exit("write error", param);
+	if (s)
+	{
+		if (write(2, s, ft_strlen(s)) == -1)
+			ft_perror_exit("write error", param);
+		if (write(2, "\n", 1) == -1)
+			ft_perror_exit("write error", param);
+	}
 	ft_free_param(param);
 	exit(1);
 }
