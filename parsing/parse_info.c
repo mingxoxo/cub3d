c61e101639d6e8eecee5b547d8cc4541a2af8b2e
jeongmin <yesuel1111@naver.com>
2023-03-16 20:37:33 +0900

    refactor: norminette error

--- parsing/parse_info.c
+++ parsing/parse_info.c
@@ -6,53 +6,40 @@
 /*   By: jeongmin <jeongmin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/14 18:53:42 by jeongmin          #+#    #+#             */
-/*   Updated: 2023/03/16 17:17:04 by jeongmin         ###   ########.fr       */
+/*   Updated: 2023/03/16 20:35:26 by jeongmin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
-static int	parse_texture(t_info *info, char **split)
+static int	parse_detail(t_info *info, char **tokens)
 {
 	char	**path;
 
 	path = NULL;
-	if (ft_strcmp(split[0], "NO") == 0)
+	if (ft_strcmp(tokens[0], "NO") == 0)
 		path = &(info->no.path);
-	else if (ft_strcmp(split[0], "SO") == 0)
+	else if (ft_strcmp(tokens[0], "SO") == 0)
 		path = &(info->so.path);
-	else if (ft_strcmp(split[0], "WE") == 0)
+	else if (ft_strcmp(tokens[0], "WE") == 0)
 		path = &(info->we.path);
-	else if (ft_strcmp(split[0], "EA") == 0)
+	else if (ft_strcmp(tokens[0], "EA") == 0)
 		path = &(info->ea.path);
+	else if (ft_strcmp(tokens[0], "F") == 0)
+		path = &(info->f.info);
+	else if (ft_strcmp(tokens[0], "C") == 0)
+		path = &(info->c.info);
 	else
 		return (ERROR);
 	if (*path)
 		return (ERROR);
-	*path = ft_strdup(split[1]);
+	*path = ft_strdup(tokens[1]);
 	if (!(*path))
 		return (ALLOC_FAILED);
 	return (SUCCESS);
 }
 
-static int	parse_color(t_info *info, char **split)
-{
-	char	**path;
-
-	path = NULL;
-	if (ft_strcmp(split[0], "F") == 0)
-		path = &(info->f.info);
-	else if (ft_strcmp(split[0], "C") == 0)
-		path = &(info->c.info);
-	if (*path)
-		return (ERROR);
-	*path = ft_strdup(split[1]);
-	if (!(*path))
-		return (ALLOC_FAILED);
-	return (SUCCESS);
-}
-
-static int	print_info(t_info *info)
+static int	check_all_info_exists(t_info *info)
 {
 	printf("-------------------\n");
 	printf("[info]\n");
@@ -84,10 +71,30 @@ static void	print_split(char **split)
 	}
 }
 
+static int	split_line(char *line, char ***tokens)
+{
+	size_t	len;
+	char	**split;
+
+	*tokens = ft_split(line, ' ');
+	split = *tokens;
+	if (!split)
+		return (ALLOC_FAILED);
+	if (!split[0] || !split[1] || split[2])
+	{
+		ft_free_two_array(tokens);
+		return (ERROR);
+	}
+	len = ft_strlen(split[1]);
+	if (len && split[1][len - 1] == '\n')
+		split[1][len - 1] = 0;
+	return (SUCCESS);
+}
+
 int	parse_info(t_list *lst, t_param *param)
 {
 	char	*line;
-	char	**split;
+	char	**tokens;
 	int		errno;
 
 	errno = SUCCESS;
@@ -97,23 +104,16 @@ int	parse_info(t_list *lst, t_param *param)
 		lst = lst->next;
 		if (line[0] == '\n')
 			continue ;
-		*(ft_strchr(line, '\n')) = 0;
-		split = ft_split(line, ' ');
-		print_split(split);
-		if (!split[0] || !split[1] || split[2])
-		{
-			ft_free_two_array(&split);
-			return (ERROR);
-		}
-		if (split[0][0] == 'F' || split[0][0] == 'C')
-			errno = parse_color(&param->info, split);
-		else
-			errno = parse_texture(&param->info, split);
-		ft_free_two_array(&split);
+		errno = split_line(line, &tokens);
+		if (errno)
+			return (errno);
+		print_split(tokens);
+		errno = parse_detail(&param->info, tokens);
+		ft_free_two_array(&tokens);
 		if (errno)
 			return (errno);
 	}
-	if (print_info(&param->info))
+	if (check_all_info_exists(&param->info))
 		return (ERROR);
 	return (SUCCESS);
 }
